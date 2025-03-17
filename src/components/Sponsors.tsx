72a9c004f37ebea8a890ca655fc14c8ccff42555
Araujo Marcos <marcosaraujo.tele@gmail.com>
2025-03-17 10:25:21 +0000

    remove award bodies

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,20 +1,20 @@
 import cert1 from '@/assets/experts/1.png'
-import cert2 from '@/assets/experts/2.png'
-import cert3 from '@/assets/experts/3.png'
-import cert4 from '@/assets/experts/4.png'
-import cert5 from '@/assets/experts/5.png'
-import cert6 from '@/assets/experts/6.png'
+// import cert2 from '@/assets/experts/2.png'
+// import cert3 from '@/assets/experts/3.png'
+// import cert4 from '@/assets/experts/4.png'
+// import cert5 from '@/assets/experts/5.png'
+// import cert6 from '@/assets/experts/6.png'
 import { useTranslation } from 'react-i18next'
 
 
 
 const sponsors: string[] = [
   cert1,
-  cert2,
-  cert3,
-  cert4,
-  cert5,
-  cert6,
+  // cert2,
+  // cert3,
+  // cert4,
+  // cert5,
+  // cert6,
 ];
 
 export const Sponsors = () => {
