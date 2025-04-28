0d46296bff0af6c8d4ea7501c63855f38d14fe9a
Araujo Marcos <marcosaraujo.tele@gmail.com>
2025-04-28 01:52:40 +0100

    commit 2025-04-29

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -6,64 +6,16 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import imageAssessor from "../assets/courses/assessor.png";
-import imageIQA from "../assets/courses/IQA.png";
-import imageTrainer from "../assets/courses/trainer.png";
-import imageBarbering from "../assets/courses/BARBERING.png";
-import imageHairdressing from "../assets/courses/HAIRDRESSING.png";
-import imageFacial from "../assets/courses/FACIAL.png";
-import imageMassage from "../assets/courses/MASSAGE.png";
-import imageEyelash from "../assets/courses/EYELASH.png";
-import imageNail from "../assets/courses/NAIL.png";
 import { useTranslation } from "react-i18next";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
 import { EmailButton } from "./EmailButton";
 import { PhonelButton } from "./PhoneButton";
 import { WhatsAppButton } from "./WhatsAppButton";
 import { InstagramButton } from "./InstagramButton";
-import { useCourses, groupCoursesI, CategoryI } from "@/lib/useCourses";
+import { useCourses, CategoryI } from "@/lib/useCourses";
 import { Tabs, TabsList, TabsContent, TabsTrigger } from "./ui/tabs";
-import { useEffect, useState } from "react";
-
-interface FeatureProps {
-  title: string;
-  heading: string;
-  description: string;
-  completeDescription: string;
-  image: string;
-}
-
-
-// const featureList: string[] = [
-//   "Dark/Light theme",
-//   "Reviews",
-//   "Features",
-//   "Pricing",
-//   "Contact form",
-//   "Our team",
-//   "Responsive design",
-//   "Newsletter",
-//   "Minimalist",
-// ];
 
 export const Features = () => {
-
-//   Courses
-//    Education
-//     "Level 3 Certificate in Assessing Vocational Achievement",
-//     "Level 4 Award in the IQA of Assessment Processes and Practices"
-//     "Level 3 Award in Education and Training",
-
-
-//     "Level 2 Certificate in Barbering"
-//     "Level 2 Certificate in Women s Hairdressing"
-//     "Level 2 Diploma for Beauty Therapists"
-//     "Level 2 Award in Facial Skincare Treatments"
-//     "Level 2 Award in Body Massage Treatments"
-//     "Level 3 Award in Eyelash Extension Application"
-//     "Level 3 Certificate in Teaching and Training"
-//     "Level 3 Diploma in Beauty Therapy Treatments"
-//    ]
 
   const { t } = useTranslation()
   const { groupedCourses } = useCourses()
@@ -111,7 +63,7 @@ export const Features = () => {
                       </CardFooter>
                     </Card>
                   </DialogTrigger>
-                  <DialogContent className="w-[80vw] min-w-[300px] max-w-[1080px]">
+                  <DialogContent className="w-[90vw] md:w-[80vw] min-w-[300px] max-w-[1080px] max-h-[80vh] overflow-scroll">
                     <DialogHeader>
                       <DialogTitle className="text-4xl">{group}</DialogTitle>
                       <DialogDescription>
@@ -152,54 +104,6 @@ export const Features = () => {
       </Tabs>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
 
-
-        {/* {groupedCourses.map(({ group, category, description, courses, image }: groupCoursesI, index) => (
-          // <Dialog>
-          //   <DialogTrigger>
-          //     <Card key={String(index)+group} className="h-full  cursor-pointer transition-transform duration-200 hover:scale-[1.02] hover:shadow-lg hover:border-primary">
-          //       <CardHeader>
-          //         <CardTitle>{group}</CardTitle>
-          //       </CardHeader>
-
-          //       <CardContent>
-          //         <b>{heading}</b>
-
-          //         <img
-          //           src={image}
-          //           alt="About feature"
-          //           className="w-[200px] lg:w-[300px] mx-auto"
-          //         />
-          //       </CardContent>
-                
-          //       <CardFooter>
-          //         {description}
-          //       </CardFooter>
-          //     </Card>
-          //   </DialogTrigger>
-          //   <DialogContent className="w-[80vw] min-w-[300px] max-w-[1080px]">
-          //     <DialogHeader>
-          //       <DialogTitle className="text-4xl">{title}</DialogTitle>
-          //       <DialogDescription>
-
-          //         <img
-          //           src={image}
-          //           alt="About feature"
-          //           className="w-[200px] lg:w-[300px] mx-auto my-10"
-          //         />
-          //         <div className="text-black dark:text-primary text-xl m-5 ">
-          //           {completeDescription.split('\b').map(e => <p className="pt-2" style={{textIndent: "2em"}}>{e}</p> )}
-          //         </div>
-          //         <div className="m-auto max-w-[320px] grid gap-4 my-4">
-          //           <EmailButton/>
-          //           <PhonelButton/>
-          //           <WhatsAppButton />
-          //           <InstagramButton />
-          //         </div>
-          //       </DialogDescription>
-          //     </DialogHeader>
-          //   </DialogContent>
-          // </Dialog>
-        ))} */}
       </div>
       
     </section>
